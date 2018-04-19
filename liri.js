e6b6f5e558722bf86fc8b31bb8be96d9ae3e93b0
madisonkoch <kochx348@umn.edu>
2018-04-18 22:33:38 -0500

    Spotify (done)

--- liri.js
+++ liri.js
@@ -8,21 +8,50 @@ const Spotify = require('node-spotify-api');
 var twitter = new Twitter(keys.twitter);
 var spotify = new Spotify(keys.spotify);
 
-
 //Retrieve Tweets
 const getTweets = function(){
     twitter.get('statuses/user_timeline',  function(error,tweets){
         if(!error)
-            console.log(tweets);
+          for (i=0; i <4; i++){
+            console.log('[' + tweets[i].created_at + ']   '+ tweets[i].text);
+          }
+        else return console.log('error:' + err);
     });
 }
 
-
 //Retrieve Spotify
-
+/**
+ * 
+ * @param {string} thisSong 
+ */
+const findSong = function(thisSong){
+  if (thisSong){
+    spotify.search({type:'track', query:thisSong, limit:1}, function(err,song){
+      if (!err){
+        console.log(`Artist: ${song.tracks.items[0].album.artists[0].name}`);
+        console.log(`Song: ${song.tracks.items[0].name}`);
+        console.log(`Preview Link: ${song.tracks.href}`);
+        console.log(`Album: ${song.tracks.items[0].album.name}`);
+      }
+      else console.log('error: '+err);
+  })
+  }
+  else {
+    spotify.search({type:'track,artist', query:'the sign, ace of base', limit:1}, function(err,song){
+      if (!err){
+        console.log(`Artist: ${song.tracks.items[0].album.artists[0].name}`);
+        console.log(`Song: ${song.tracks.items[0].name}`);
+        console.log(`Preview Link: ${song.tracks.href}`);
+        console.log(`Album: ${song.tracks.items[0].album.name}`);
+      }
+      else console.log('error: '+err);
+  })
+  }
+}
 
 //Liri Commands
 var inputCommand = (process.argv[2]);
+var inputSearchTerm = (process.argv[3])
 
 switch (inputCommand) {
     case "my-tweets":
@@ -30,7 +59,7 @@ switch (inputCommand) {
       break;
     
     case "spotify-this-song":
-      console.log('spots');
+      findSong(inputSearchTerm);
       break;
     
     case "movie-this":
